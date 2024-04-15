cfbf83cb323648cb4a2df4f4ff9fc4fbcf096700
Misha <164007633+H-R-G-R@users.noreply.github.com>
2024-04-15 18:00:14 +0300

    update Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,26 +2,26 @@ import css from './Profile.module.css';
 
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div className={css.wrapperProfile}>
-      <div className={css.infoProfile}>
-        <img src={image} alt={name} width={180} height={180} />
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={image} alt={name} className={css.avatar} />
         <p className={css.name}>{name}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.stats}>
+        <li className={css.statsItem}>
+          <span className={css.label}>Followers</span>
+          <span className={css.value}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.statsItem}>
+          <span className={css.label}>Views</span>
+          <span className={css.value}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.statsItem}>
+          <span className={css.label}>Likes</span>
+          <span className={css.value}>{stats.likes}</span>
         </li>
       </ul>
     </div>
